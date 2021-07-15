cd00a3726a5b234bcd52528cf73be831b91ba85a
Julien LE BREN <julien@lebren.fr>
2021-07-15 11:05:31 +0200

    Creation of package 'firestore_service'

---
+++ packages/firestore_service/lib/firestore_service.dart
@@ -0,0 +1,22 @@
+library firestore_service;
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final firestoreServiceProvider =
+    Provider<FirestoreService>((ref) => FirestoreService.instance);
+
+class FirestoreService {
+  FirestoreService._();
+
+  static final instance = FirestoreService._();
+  static final firestore = FirebaseFirestore.instance;
+
+  Stream<List<T>> collectionStream<T>({
+    required Query<T> query,
+  }) {
+    return query.snapshots().map((snapshot) {
+      return snapshot.docs.map((snapshot) => snapshot.data()).toList();
+    });
+  }
+}
