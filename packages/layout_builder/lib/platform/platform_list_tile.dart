5e3319d8c0579e2676851d371910c93a005ff3f5
Julien LE BREN <julien@lebren.fr>
2021-07-19 16:15:06 +0200

    Few changes in ListTile

--- packages/layout_builder/lib/platform/platform_list_tile.dart
+++ packages/layout_builder/lib/platform/platform_list_tile.dart
@@ -180,7 +180,7 @@ class ListTileContents extends ConsumerWidget {
         minHeight: 48.0,
       ),
       child: Container(
-        padding: EdgeInsets.only(top: 10, bottom: 6, left: 15, right: 8),
+        padding: EdgeInsets.only(top: 8, bottom: 8, left: 15, right: 8),
         child: child ??
             Row(
               mainAxisSize: MainAxisSize.max,
