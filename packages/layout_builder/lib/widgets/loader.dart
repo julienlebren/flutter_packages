3b3548521f432cecf8e9c2a25e31319ae53785bd
Julien LE BREN <julien@lebren.fr>
2021-07-23 11:07:28 +0200

    Added Loader

---
+++ packages/layout_builder/lib/widgets/loader.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:layout_builder/platform/platform.dart';
+
+class Loader extends StatelessWidget {
+  const Loader({
+    this.delayBeforeDisplay = 1,
+  }) : super();
+
+  final int delayBeforeDisplay;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Future.delayed(Duration(seconds: delayBeforeDisplay), () {}),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          return Container();
+        }
+      },
+    );
+  }
+}
+
+class ScaffoldLoader extends StatelessWidget {
+  const ScaffoldLoader({
+    this.delayBeforeDisplay = 1,
+  }) : super();
+
+  final int delayBeforeDisplay;
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformScaffold(
+      body: Loader(
+        delayBeforeDisplay: delayBeforeDisplay,
+      ),
+    );
+  }
+}
