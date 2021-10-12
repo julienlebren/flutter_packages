1e3cbba4505e1e6c481bce823f98d1f4428f0845
Julien LE BREN <julien@lebren.fr>
2021-10-12 09:38:10 +0200

    Added toLocaleShortDate

--- packages/extensions/lib/date.dart
+++ packages/extensions/lib/date.dart
@@ -3,6 +3,9 @@ part of 'extensions.dart';
 extension DateTimeX on DateTime {
   String get toHour => DateFormat('Hm').format(this);
 
+  String toLocaleShortDate(BuildContext context) =>
+      DateFormat.yMd(Localizations.localeOf(context).toString()).format(this);
+
   String toLocaleDate(BuildContext context) =>
       DateFormat.yMMMMd(Localizations.localeOf(context).toString())
           .format(this);
