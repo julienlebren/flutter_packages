370d21295889e3ca7826228bcf557f01166c73fb
Julien LE BREN <julien@lebren.fr>
2021-10-13 10:15:17 +0200

    First commit of subscription_service

---
+++ packages/subscription_service/lib/subscription_service.dart
@@ -0,0 +1,75 @@
+library subscription_service;
+
+import 'package:purchases_flutter/purchases_flutter.dart';
+
+class SubscriptionService {
+  SubscriptionService({
+    required this.purchasesApiKey,
+    required this.entitlementId,
+    required this.userId,
+    required this.processHandler,
+  });
+
+  final String purchasesApiKey;
+  final String entitlementId;
+  final String userId;
+
+  final Function(
+    bool isSubscribed,
+    DateTime? expirationDate,
+  ) processHandler;
+
+  Package? subscription;
+
+  Future<void> setup() async {
+    await Purchases.setup(
+      purchasesApiKey,
+      appUserId: userId,
+      observerMode: false,
+    );
+  }
+
+  Future<void> fetchOfferings() async {
+    if (subscription != null) return;
+    Offerings offerings = await Purchases.getOfferings();
+    print("offerings: $offerings");
+    final offering = offerings.current;
+    if (offering != null) {
+      if (offering.annual != null) {
+        subscription = offering.annual!;
+        print("subscription: $subscription");
+      }
+    }
+  }
+
+  Future<void> purchase() async {
+    if (subscription == null) return;
+    PurchaserInfo purchaserInfo =
+        await Purchases.purchasePackage(subscription!);
+    _processInfo(purchaserInfo);
+  }
+
+  Future<void> refreshSubscription() async {
+    PurchaserInfo purchaserInfo = await Purchases.getPurchaserInfo();
+    _processInfo(purchaserInfo);
+  }
+
+  Future<void> restorePurchase() async {
+    PurchaserInfo purchaserInfo = await Purchases.restoreTransactions();
+    _processInfo(purchaserInfo);
+  }
+
+  Future<void> _processInfo(PurchaserInfo info) async {
+    final entitlementInfo = info.entitlements.all[entitlementId];
+    if (entitlementInfo == null) return;
+    print("entitlementInfo: $entitlementInfo");
+
+    final isSubscribed = entitlementInfo.isActive;
+    DateTime? expirationDate = entitlementInfo.expirationDate != null
+        ? DateTime.parse(entitlementInfo.expirationDate!)
+        : null;
+
+    print("expirationDate: $expirationDate");
+    processHandler(isSubscribed, expirationDate);
+  }
+}
