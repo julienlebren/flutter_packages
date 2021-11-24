0812ae3875205dc3dea33800ca3737b108f5c1fb
Julien LE BREN <julien@lebren.fr>
2021-11-24 10:22:48 +0100

    Added ListDivider

--- packages/layout_builder/lib/platform/platform_list_view.dart
+++ packages/layout_builder/lib/platform/platform_list_view.dart
@@ -24,20 +24,29 @@ class PlatformListView extends PlatformWidgetBase<ListView, ListView> {
 
   @override
   ListView createCupertinoWidget(BuildContext context, WidgetRef ref) {
-    final listViewTheme = ref.watch(listViewThemeProvider);
-    final appTheme = ref.watch(appThemeProvider);
     return ListView.separated(
       physics: scrollPhysics,
       padding: EdgeInsets.zero,
       itemCount: itemCount,
       itemBuilder: itemBuilder,
       shrinkWrap: shrinkWrap,
-      separatorBuilder: (_, __) => Padding(
-        padding: EdgeInsets.only(left: listViewTheme.separatorPadding),
-        child: Divider(
-          color: appTheme.borderColor,
-          height: 0.5,
-        ),
+      separatorBuilder: (_, __) => const ListDivider(),
+    );
+  }
+}
+
+class ListDivider extends ConsumerWidget {
+  const ListDivider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final listViewTheme = ref.watch(listViewThemeProvider);
+    final appTheme = ref.watch(appThemeProvider);
+    return Padding(
+      padding: EdgeInsets.only(left: listViewTheme.separatorPadding),
+      child: Divider(
+        color: appTheme.borderColor,
+        height: 0.5,
       ),
     );
   }
