ae83060a7286d8b1fa2be2c800223452cc1406e3
Julien LE BREN <julien@lebren.fr>
2022-03-03 11:16:24 +0100

    Tests

--- packages/sign_in/lib/presentation/router/sign_in_router.dart
+++ packages/sign_in/lib/presentation/router/sign_in_router.dart
@@ -111,7 +111,7 @@ class SignInNavigator extends ConsumerWidget {
             if (signInSupplier == SignInSupplier.apple ||
                 signInSupplier == SignInSupplier.google ||
                 signInSupplier == SignInSupplier.facebook) {
-              navigator.pushNamed(SignInRoutes.signInUserInfoPage);
+              navigator.pushNamed(SignInRoutes.signInLandingPage);
             }
           }
         },
