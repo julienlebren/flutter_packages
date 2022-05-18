3b5390171b430e49051cdd6c1e6d71679c086301
Julien LE BREN <julien@lebren.fr>
2022-05-18 11:27:52 +0200

    Tests

--- packages/sign_in/lib/presentation/views/sign_in/sign_in_unknown_page.dart
+++ packages/sign_in/lib/presentation/views/sign_in/sign_in_unknown_page.dart
@@ -5,8 +5,10 @@ class SignInUnknownPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Unknown page"),
+    return const PlatformScaffold(
+      body: Center(
+        child: Text("Unknown page"),
+      ),
     );
   }
 }
