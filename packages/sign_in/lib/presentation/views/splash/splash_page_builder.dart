e2711c77c974079ab7f2acf10423080704a44180
Julien LE BREN <julien@lebren.fr>
2022-05-19 09:53:01 +0200

    Tests

--- packages/sign_in/lib/presentation/views/splash/splash_page_builder.dart
+++ packages/sign_in/lib/presentation/views/splash/splash_page_builder.dart
@@ -14,10 +14,10 @@ class SplashPageBuilder extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authArguments = ref.read(authSettingsProvider);
-    final authState = ref.watch(authStateProvider(authArguments));
+    final settings = ref.read(authSettingsProvider);
+    final authState = ref.watch(authStateProvider(settings));
 
-    ref.listen<AuthState>(authStateProvider(authArguments), (
+    /*ref.listen<AuthState>(authStateProvider(authArguments), (
       previousState,
       authState,
     ) {
@@ -45,6 +45,25 @@ class SplashPageBuilder extends ConsumerWidget {
         },
         orElse: () => null,
       );
+    });*/
+
+    ref.listen<dynamic>(settings.needUserInfoProvider!, (
+      previous,
+      needUserInfo,
+    ) {
+      if (needUserInfo) {
+        final supplier = ref.watch(signInSupplierProvider);
+        if (supplier != null && !supplier.isThirdParty) {
+          final navigator = SignInNavigatorKeys.modal.currentState!;
+          navigator.pushReplacementNamed(SignInRoutes.signInUserInfoPage);
+        } else {
+          final navigator = Navigator.of(context, rootNavigator: true);
+          Future.delayed(const Duration(milliseconds: 200), () {
+            navigator.pushNamed(SignInRoutes.signInUserInfoPage,
+                arguments: true);
+          });
+        }
+      }
     });
 
     return authState.maybeWhen(
