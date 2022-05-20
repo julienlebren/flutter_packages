34f3a25f6a072223ee7a6e9d00e6c5413e469ab7
Julien LE BREN <julien@lebren.fr>
2022-05-20 18:05:10 +0200

    Added extension on List

--- packages/extensions/lib/iterable.dart
+++
@@ -1,6 +0,0 @@
-part of 'extensions.dart';
-
-extension IterableX<E> on Iterable<E> {
-  Iterable<E> sortedBy(Comparable key(E e)) =>
-      toList()..sort((a, b) => key(a).compareTo(key(b)));
-}
---
+++ packages/extensions/lib/list.dart
@@ -0,0 +1,9 @@
+part of 'extensions.dart';
+
+extension ListX<E> on List<E> {
+  List<E> sortedBy(Comparable key(E e)) {
+    var tmpList = this;
+    tmpList.sort((a, b) => key(a).compareTo(key(b)));
+    return tmpList;
+  }
+}
