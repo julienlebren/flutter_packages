aa9a80fb175c81af6c840b7d3b4c677174390cb1
Julien LE BREN <julien@lebren.fr>
2022-05-24 16:12:45 +0200

    Added onTap on AnonymousBadge

--- packages/sign_in/lib/presentation/widgets/anonymous_badge.dart
+++ packages/sign_in/lib/presentation/widgets/anonymous_badge.dart
@@ -7,32 +7,36 @@ class AnonymousBadge extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = ref.watch(signInLocalizationsProvider);
 
-    return FormSection(
-      child: Padding(
-        padding: EdgeInsets.only(top: isMaterial() ? 10 : 0),
-        child: Container(
-          color: Colors.amber,
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          child: SizedBox(
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Icon(
-                  Icons.warning,
-                  color: Colors.black,
-                  size: 40,
-                ),
-                const SizedBox(width: 10),
-                Flexible(
-                  child: Text(
-                    l10n.settingsNoAccount,
-                    style: const TextStyle(color: Colors.black, fontSize: 17),
-                    maxLines: 4,
-                    overflow: TextOverflow.ellipsis,
+    return GestureDetector(
+      onTap: () =>
+          Navigator.pushNamed(context, SettingsRoutes.settingsAccountPage),
+      child: FormSection(
+        child: Padding(
+          padding: EdgeInsets.only(top: isMaterial() ? 10 : 0),
+          child: Container(
+            color: Colors.amber,
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: SizedBox(
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.warning,
+                    color: Colors.black,
+                    size: 40,
                   ),
-                ),
-              ],
+                  const SizedBox(width: 10),
+                  Flexible(
+                    child: Text(
+                      l10n.settingsNoAccount,
+                      style: const TextStyle(color: Colors.black, fontSize: 17),
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
