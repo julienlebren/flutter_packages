df5c2bce1f0eb07e181abf956a15c0d81b5b7a3c
Julien LE BREN <julien@lebren.fr>
2022-06-24 14:27:33 +0200

    Added disclosure to anonymous badge

--- packages/sign_in/lib/presentation/widgets/anonymous_badge.dart
+++ packages/sign_in/lib/presentation/widgets/anonymous_badge.dart
@@ -38,6 +38,7 @@ class AnonymousBadge extends ConsumerWidget {
                       overflow: TextOverflow.ellipsis,
                     ),
                   ),
+                  const PlatformDisclosureIndicator(),
                 ],
               ),
             ),
