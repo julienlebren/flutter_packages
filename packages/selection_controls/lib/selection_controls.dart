a900d18df94591e390e3c5186507ee7d0e6bc511
Julien LE BREN <julien@lebren.fr>
2022-09-16 18:30:49 +0200

    Created selection_controls

---
+++ packages/selection_controls/lib/selection_controls.dart
@@ -0,0 +1,11 @@
+library selection_controls;
+
+import 'dart:math' as math;
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/cupertino.dart';
+
+part 'src/custom_text_selection.dart';
+part 'src/selectable_text_highlighter.dart';
