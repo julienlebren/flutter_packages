968d97765fc84a1f1034e578ac8017b107b77851
Julien LE BREN <julien@lebren.fr>
2022-10-06 08:33:39 +0200

    Added @riverpod annotation

--- packages/ad_manager/lib/ad_manager.dart
+++ packages/ad_manager/lib/ad_manager.dart
@@ -6,13 +6,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'src/ad_settings.dart';
 part 'src/inline_adaptive.dart';
 part 'src/intersticial_container.dart';
 
 part 'ad_manager.freezed.dart';
+part 'ad_manager.g.dart';
 
+/*
 final adSettingsProvider = Provider<AdSettings>(
   (_) => throw UnimplementedError(),
 );
+*/
+
+@riverpod
+AdSettings adSettings(AdSettingsRef ref) => throw UnimplementedError();
