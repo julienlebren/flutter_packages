72fccb3c7569d6cfd9c598ba1a1f89d15b9aafb7
Julien LE BREN <julien@lebren.fr>
2022-10-18 15:04:43 +0200

    Testing

--- packages/purchases/lib/purchases.dart
+++ packages/purchases/lib/purchases.dart
@@ -2,6 +2,7 @@ library subscription_service;
 
 import 'dart:io';
 
+import 'package:extensions/extensions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
