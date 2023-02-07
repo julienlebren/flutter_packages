de21703441f3ecbb2c289dcbf1d66f7af53ca0d8
Julien LE BREN <julien@lebren.fr>
2023-02-07 11:18:39 +0100

    Tests

--- packages/layout_builder/lib/platform/platform_widget_base.dart
+++ packages/layout_builder/lib/platform/platform_widget_base.dart
@@ -14,7 +14,7 @@ abstract class PlatformWidgetBase<
       switch (defaultTargetPlatform) {
         case TargetPlatform.iOS:
         case TargetPlatform.macOS:
-          return createCupertinoWidget(context, ref);
+          return createMaterialWidget(context, ref);
         case TargetPlatform.android:
           return createMaterialWidget(context, ref);
         default:
